40923a04376ca18cc531311bed6d58d6845fb702
Ajay Andrew <ajayandrew18@gmail.com>
2026-03-08 21:01:50 +0530

    updated

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const USER = { email: "laddu@netflix.com", password: "Thangam@91" };
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (email === USER.email && password === USER.password) {
+    res.json({ success: true, message: "Login successful" });
+  } else {
+    res.json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
