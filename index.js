bb986cc24e079297f9a9218bffbb7b252a331f43
Ajay Andrew <ajayandrew18@gmail.com>
2026-03-08 23:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.post("/login", (req, res) => {
     res.json({ success: false, message: "Invalid credentials" });
   }
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
